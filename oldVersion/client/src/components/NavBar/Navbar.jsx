eea9f21e14914b50fd92964df579a5a555496e1e
Lewi-5 <124799335+Lewi-5@users.noreply.github.com>
2023-10-16 09:08:45 -0400

    daily scrum 10/16

--- oldVersion/client/src/components/NavBar/Navbar.jsx
+++ oldVersion/client/src/components/NavBar/Navbar.jsx
@@ -5,7 +5,35 @@ import { AuthContext } from "../../contexts/AuthContext";
 
 function Navbar() {
 
-  const {user} = useContext(AuthContext);
+  const {user ,dispatch} = useContext(AuthContext);
+
+  const handleLogout = async () => {
+      dispatch({type: "LOGOUT"})
+    try {
+      const response = await axios.post('http://localhost:8080/api/user/logout')
+      dispatch({type: "LOGIN_SUCCESS", payload: response.data})
+      console.log(response);
+      setLoginStatus(true);
+      const fullName = response.data.fullname;
+      setLoginMsg(`Welcome back ${fullName}`);
+      if (response.status === 200){
+        setTimeout(() => {
+          navigate("/");
+        }, 4000)
+      }
+  } catch (error) {
+    setErrorStatus(true);
+    dispatch({type: "LOGIN_FAILURE", payload: error.response.data})
+      if (error.response) {
+        setLoginMsg(error.response.data);
+          console.error(error.response.data);
+          console.error(error.response.status);
+          console.error(error.response.headers);
+        } else {
+          console.error(`Error: ${error.message}`);
+        }
+      }
+  }
 
   return (
     <div className="navbar">
