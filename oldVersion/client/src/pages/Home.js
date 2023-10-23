38028ecd6b94a89142e40551c28619efe7069b89
Lewi-5 <124799335+Lewi-5@users.noreply.github.com>
2023-10-22 20:47:38 -0400

    remove home page links

--- oldVersion/client/src/pages/Home.js
+++ oldVersion/client/src/pages/Home.js
@@ -107,31 +107,7 @@ const Home = () => {
           )}
         </div>
       </div>
-      <div className="links">
-        {/* Public Links */}
-        <p>TESTING</p>
-        <Link to="/login">
-          {" "}
-          <button>Login</button>
-        </Link>
-        <Link to="/register"> register</Link>
-        <Link to="/types"> types</Link>
-        <Link to="/unauthorized">unauthorized</Link>
-        {/* Private Links */}
-        <Link to="/profile">profile</Link>
-        <Link to="/confirm">confirm</Link>
-        <Link to="/admin/bookings">admin bookings</Link>
-        <Link to="/admin/data">admin data</Link>
-        <Link to="/admin/reviews">admin reviews</Link>
-        <Link to="/admin/users">admin users</Link>
-        <Link to="/admin/vehicles">admin vehicles</Link>
 
-        {/* Catch all */}
-        <Link to="*" element={<Missing />}>
-          Missing{" "}
-        </Link>
-      </div>
-      
     </>
   );
 };
